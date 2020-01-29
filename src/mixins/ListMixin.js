72add021b6f7e8c1a1affb24fc1e48fa8cd2e19b
seommin <seomm1n@naver.com>
2020-01-29 14:58:47 +0900

    Mixin.js 추가

---
+++ src/mixins/ListMixin.js
@@ -0,0 +1,13 @@
+import bus from "../utils/bus"
+
+export default {
+    created() {
+        bus.$emit("start:spinner");
+        this.$store
+            .dispatch("FETCH_LIST", this.$route.name)
+            .then(() => {
+                bus.$emit("end:spinner");
+            })
+            .catch(error => console.log(error));
+    },
+}
