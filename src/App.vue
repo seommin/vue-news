affc16c66f4b9fd674c91368036043ca93895f7f
seommin <seomm1n@naver.com>
2020-01-29 09:28:47 +0900

    기존 작업 내용 불러오기

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <tool-bar></tool-bar>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+import ToolBar from "./components/ToolBar.vue";
+export default {
+  components: {
+    ToolBar
+  }
+};
+</script>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+
+a {
+  color: #35495e;
+  text-decoration: none;
+}
+
+a:hover {
+  color: #42b883;
+  text-decoration: underline;
+}
+
+a.router-link-exact-active {
+  text-decoration: underline;
+}
+
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+
+.page-ender,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ src/routes/index.js
@@ -0,0 +1,46 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import NewsView from "../views/NewsView.vue";
+import AskView from "../views/AskView.vue";
+import JobsView from "../views/JobsView.vue";
+import UserView from "../views/UserView.vue";
+import ItemView from "../views/ItemView.vue";
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      redirect: "/news"
+    },
+    {
+      // path: url 주소
+      path: "/news",
+      name: "news",
+      // compenent: 보여줄 컴포넌트
+      component: NewsView
+    },
+    {
+      path: "/ask",
+      name: "ask",
+      component: AskView
+    },
+    {
+      path: "/jobs",
+      name: "jobs",
+      component: JobsView
+    },
+    {
+      path: "/user/:id",
+      name: "user",
+      component: UserView
+    },
+    {
+      path: "/item/:id",
+      name: "item",
+      component: ItemView
+    }
+  ]
+});
