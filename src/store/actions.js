18ab7aa52ffb853710bbe645d8079f164359b246
seommin <seomm1n@naver.com>
2020-01-30 10:31:26 +0900

    Mixin 및 라우터 네비게이션 가드 적용

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,12 +6,15 @@ import {
 
 export default {
   FETCH_LIST({ commit }, pageName) {
-    fetchList(pageName)
-      .then(({ data }) => commit('SET_LIST', data))
+    return fetchList(pageName)
+      .then(({ data }) => {
+        commit('SET_LIST', data);
+        return data;
+      })
       .catch(error => console.log(error));
   },
   FETCH_USER({ commit }, name) {
-    fetchUserInfo(name)
+    return fetchUserInfo(name)
       .then(({ data }) => {
         commit("SET_USER", data);
         return data;
@@ -19,7 +22,7 @@ export default {
       .catch(error => console.log(error));
   },
   FETCH_ITEM({ commit }, id) {
-    fetchCommentItem(id)
+    return fetchCommentItem(id)
       .then(({ data }) => {
         commit("SET_ITEM", data);
         return data;
