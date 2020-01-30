fa232823cd755ea0cb823dd635cded7971b3f0fb
seommin <seomm1n@naver.com>
2020-01-30 11:07:26 +0900

    firebase 추가

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
+import VueFire from 'vuefire';
 import App from "./App.vue";
 import { router } from "./routes/index.js";
 import { store } from "./store/index.js";
+
+Vue.use(VueFire);
 
 Vue.config.productionTip = false;
 
